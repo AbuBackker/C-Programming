edd5ed8c11cfe153933090e2b8be15163e89adea
unknown <abubackker11@gmail.com>
2021-02-15 15:57:26 +0600

    first commit

---
+++ c-programming.c
@@ -0,0 +1,12 @@
+// Sum of Two Numbers:
+
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+
+     int a = 50, b = 60, sum;
+     sum = a + b;
+     printf("%d + %d = %d", a, b, sum);
+     return 0;
+}
